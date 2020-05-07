1d8377b5c80d6dfb447930f832e37c47970d330e
Denis Martin Budinski <budinski.denis@gmail.com>
2020-05-07 18:12:57 +0200

    Added pages Registration and Login

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -3,10 +3,6 @@ import React from "react";
 const LogIn = (props) => {
     return(
         <>
-            <h2>Login</h2>
-            <input type="text" placeholder="Username"></input>
-            <input type="text" placeholder="Password"></input>
-            <button>Login</button>
         </>
     );
 }
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,29 +1,11 @@
-import React, {useState} from "react";
-import { Redirect } from 'react-router-dom';
+import React from "react";
  
 
 import {PostRegisterUser} from "../services/apiWorks";
 
 const Registration = (props) => {
-    const [login, setLogin] = useState('');
-    const [messeg, setmMsseg] = useState('');
-    
-    function RegisterUser(){
-        PostRegisterUser().then(res => setLogin(res.ok));
-        login ? setmMsseg('') : setmMsseg('Greška prilikom registracije!');
-    }
-
-    function ClearMesseg(){
-        setmMsseg('');
-    }
-
     return(
         <>
-            <h2>Registration </h2>
-            <input type="text" id="userName" placeholder="Username" onFocus={ClearMesseg}></input>
-            <input type="text" id="password" placeholder="Password" onFocus={ClearMesseg}></input>
-            <button onClick={RegisterUser}>Register</button>
-            {login ? <Redirect to="/login"/> : <p>{messeg}</p>}
         </>
     );
 }
